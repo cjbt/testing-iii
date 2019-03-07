e6188ba297b5d5e223e39c85a7c530ad3eb0862b
cecil john tantay <cjbtantay@gmail.com>
2019-03-07 15:25:04 -0800

    add 2 more test for dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,8 +1,11 @@
 // Test away
 import React from 'react';
 import { render } from 'react-testing-library';
-import Dashboard from './Dashboard';
 import renderer from 'react-test-renderer';
+
+import Dashboard from './Dashboard';
+import Display from '../display/Display';
+import Controls from '../controls/Controls';
 
 describe('<Dashboard />', () => {
   it('should render without any crashes', () => {
@@ -12,5 +15,11 @@ describe('<Dashboard />', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<Dashboard />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  it('<Controls /> should render', () => {
+    render(<Controls />);
+  });
+  it('<Display /> should render', () => {
+    render(<Display />);
   });
 });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+
+describe('<Display />', () => {
+  it('should check whether there is a class', () => {});
+
+  // class is red
+  // class is green
+  // function is firing
+  // locked or unlocked
+  // closed or open
+});
