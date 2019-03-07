812c92f49711e15ce7da84a3ad2d975e14009fd7
cecil john tantay <cjbtantay@gmail.com>
2019-03-07 14:29:52 -0800

    finish two test in dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -2,10 +2,15 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 import Dashboard from './Dashboard';
+import renderer from 'react-test-renderer';
 
 describe('<Dashboard />', () => {
   it('should render without any crashes', () => {
     const helpers = render(<Dashboard />);
     console.log('helpers', helpers);
+  });
+  it('renders correctly', () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
